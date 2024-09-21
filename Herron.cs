31b6aab95ee58a7a05534e930a8a78d98ccfef93
dfntlyNotNate <nathanielillescas1@gmail.com>
2024-09-21 11:43:19 +0800

    Commit 1

---
+++ Herron.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Triangle
+{
+    public static double TriangleArea(double a, double b, double c)
+    {
+
+        double s = (a + b + c) / 2;
+
+
+        double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+        return area;
+    }
+
+    public static void Main(string[] args)
+    {
+
+        double a = 3, b = 4, c = 5;
+        double area = TriangleArea(a, b, c);
+        Console.WriteLine($"The area of the triangle is {area}");
+    }
+}
---
+++ Leap Year.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class LeapYearChecker
+{
+    public static bool IsLeapYear(int year)
+    {
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter a year:");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+
+        if (IsLeapYear(year))
+        {
+            Console.WriteLine($"{year} is a leap year.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} is not a leap year.");
+        }
+    }
+}
